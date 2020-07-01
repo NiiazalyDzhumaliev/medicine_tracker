20cf13785a9e1d2487741da4afd103f20326dad7
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-01 18:29:59 +0600

    Add methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
-end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        log_in(@user)
+        redirect_to root_path
+      else
+        render :new
+      end
+    end 
+  
+  
+    def show
+      if sign_in?
+        @user = current_user
+        @total = user_injection
+      else
+        @user = nil
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :username)
+    end
+  end
