830c57450fb53c49cbf4cb02a61bddf810d5c0f5
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-01 18:24:41 +0600

    Add routes to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :show]
   get  '/login', to: 'sessions#new'
   post  '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
