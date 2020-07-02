d42c830ef38b853409b90ee5e773100f670f06d8
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 13:06:47 +0600

    Add medicine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_130758) do
+ActiveRecord::Schema.define(version: 2020_07_01_134414) do
+
+  create_table "medicines", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "name"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
