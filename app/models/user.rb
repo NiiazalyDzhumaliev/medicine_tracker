8580b2b1ba4533bf86f1f4ada52719a491835f30
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 14:11:43 +0600

    Add user id to groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :medicines, foreign_key: :author_id
+    has_many :groups
     validates :username, presence: true, uniqueness: true, length: { in: 2..10 }
     validates :name, presence: true
 end
