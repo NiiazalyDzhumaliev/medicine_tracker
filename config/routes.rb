96e6ec14a1465015b7056d7fabc3d8a48fb38874
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 13:58:02 +0600

    Make login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get  '/login', to: 'sessions#new'
   post  '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   root "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
