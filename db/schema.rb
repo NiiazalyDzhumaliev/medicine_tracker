f9f4e99e621cb8c816961eccc83dbb8f9b512d6f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 14:10:54 +0600

    Add user id to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_080335) do
+ActiveRecord::Schema.define(version: 2020_07_02_081023) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "medicines", force: :cascade do |t|
@@ -36,5 +38,6 @@ ActiveRecord::Schema.define(version: 2020_07_02_080335) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "medicines", "groups"
 end
