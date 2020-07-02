18408d06995633b8401891b80360dc42f2deec6f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 17:54:16 +0600

    Add methods to medicine controller

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,5 +1,24 @@
 class MedicinesController < ApplicationController
     def new
         @medicine = Medicine.new
-    end    
+    end
+
+    def create
+        @medicine = Medicine.new(comment_params)
+        @medicine.group_id = params[:group_id]
+        if @medicine.save
+          flash[:success] = "Object successfully created"
+          redirect_to group_path(@medicine.group)
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private
+
+    def medicine_params
+        params.require(:medicine).permit(:name, :amount)
+    end
+    
 end
