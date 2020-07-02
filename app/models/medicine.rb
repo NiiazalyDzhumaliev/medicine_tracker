a9d999db814baf4930a83a91ef826a611ee510c3
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 14:07:27 +0600

    Add validations to medicine

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,3 +1,5 @@
 class Medicine < ApplicationRecord
     belongs_to :author, foreign_key: :author_id, class_name: 'User'
+    validates :name, presence: true, length: { in: 2..15 }
+    validates :amount, presence: true, numericality: { less_than_or_equal_to: 100, only_integer: true }
 end
