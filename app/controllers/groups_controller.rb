4f4aff1045a267a1165e26ec01880bd4f3e46978
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-02 14:58:34 +0600

    Creating groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,28 @@
 class GroupsController < ApplicationController
-    def index
-        @groups = Group.where(:user_id => current_user.id)
+
+    def new
+        @group = Group.new
     end
-    
+
+    def index
+        @groups = Group.where(:user_id => current_user.id).order("created_at DESC")
+    end
+
+    def create
+        @group = current_user.groups.new(group_params)
+        if @group.save
+          flash[:success] = "Object successfully created"
+          redirect_to groups_path
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name, :icon)
+    end
+        
 end
