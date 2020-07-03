d94f2f4439f86e3f1d5fe1c542d3f8d309d69f94
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-03 18:55:25 +0600

    Revise medicines controller

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,31 +1,25 @@
 class MedicinesController < ApplicationController
-    def new
-        @medicine = Medicine.new
-        @group = Group.find(params[:group_id])
-    end
-    
-    def index
-      @medicines = Medicine.where(:author_id => current_user.id).order("created_at DESC")
-    end
-    
+  def new
+    @medicine = Medicine.new
+end
 
-    def create
-        @group = Group.find(params[:group_id])
-        @medicine = @group.medicines.new(medicine_params)
-        @medicine.author_id = current_user.id
-        if @medicine.save
-          flash[:success] = "Object successfully created"
-          redirect_to root_path
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
+def index
+    @medicines = Medicine.where(:author_id => current_user.id).order("created_at DESC")
+end
 
-    private
 
-    def medicine_params
-        params.require(:medicine).permit(:name, :amount, :author_id, :group_id)
+def create
+    @medicine = current_user.medicines.new(medicine_params)
+    if @medicine.save
+      flash[:success] = "Object successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
     end
-    
+end
+
+def medicine_params
+    params.require(:medicine).permit(:name, :amount)
+end
 end
