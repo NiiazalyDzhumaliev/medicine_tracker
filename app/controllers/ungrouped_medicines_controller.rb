917261a85f2d9a1ea1c90bae5a32258013335aca
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-03 16:00:07 +0600

    Add ungrouped medicine form

--- app/controllers/ungrouped_medicines_controller.rb
+++ app/controllers/ungrouped_medicines_controller.rb
@@ -1,2 +1,26 @@
 class UngroupedMedicinesController < ApplicationController
+    def new
+        @ungrouped_medicine = UngroupedMedicine.new
+    end
+
+    def index
+        @ungrouped_medicines = UngroupedMedicine.where(:user_id => current_user.id).order("created_at DESC")
+    end
+
+
+    def create
+        @ungrouped_medicine = current_user.ungrouped_medicines.new(ungrouped_medicine_params)
+        if @ungrouped_medicine.save
+          flash[:success] = "Object successfully created"
+          redirect_to ungrouped_medicines_path
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def ungrouped_medicine_params
+        params.require(:ungrouped_medicine).permit(:name, :amount)
+    end
+    
 end
