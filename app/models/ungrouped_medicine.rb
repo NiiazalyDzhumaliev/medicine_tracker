f587c287013058a743f05c64b1d3458347523010
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-03 15:38:59 +0600

    Generate ungroup medicine

---
+++ app/models/ungrouped_medicine.rb
@@ -0,0 +1,3 @@
+class UngroupedMedicine < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_081023) do
+ActiveRecord::Schema.define(version: 2020_07_03_093810) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_07_02_081023) do
     t.index ["group_id"], name: "index_medicines_on_group_id"
   end
 
+  create_table "ungrouped_medicines", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ungrouped_medicines_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -40,4 +49,5 @@ ActiveRecord::Schema.define(version: 2020_07_02_081023) do
 
   add_foreign_key "groups", "users"
   add_foreign_key "medicines", "groups"
+  add_foreign_key "ungrouped_medicines", "users"
 end
