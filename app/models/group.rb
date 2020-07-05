22a1d6956ec3507a50a4e9c94707b818054b855a
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-05 13:32:05 +0600

    Add image to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
     belongs_to :user
     has_many :medicines
     validates :name, presence: true, length: { in: 2..15 }
+    validates :icon, format: { with: /.+\.(gif|jpe?g|png)\z/, message: 'Please use .gif, .jpeg, .jpg or .png' }
+    validates :icon, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'Please use a valid URL' }
 end
