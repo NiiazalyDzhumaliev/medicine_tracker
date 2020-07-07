de4e95fdf69d3ef29edb803624f35b0107afae27
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-07-07 11:35:22 +0600

    Avoid n+1 problem

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -4,10 +4,10 @@ class Medicine < ApplicationRecord
   validates :name, presence: true, length: { in: 2..15 }
   validates :amount, presence: true, numericality: { less_than_or_equal_to: 1000, only_integer: true }
   scope :ungrouped_ones, lambda { |user|
-                           where(author_id: user.id).where(group_id: nil).order('created_at DESC')
+                           where(author_id: user.id).where(group_id: nil).order('created_at DESC').includes(:group)
                          }
 
   scope :grouped_ones, lambda { |user|
-    where(author_id: user.id).where.not(group_id: nil).order('created_at DESC')
+    where(author_id: user.id).where.not(group_id: nil).order('created_at DESC').includes(:group)
   }
 end
